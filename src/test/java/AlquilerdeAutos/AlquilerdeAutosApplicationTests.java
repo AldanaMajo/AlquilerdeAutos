1b84d5e021b7ec239e17691a02e5276d82748575
AldanaMajo <majoaldana178@gmail.com>
2026-08-14 16:27:57 -0600

    Creacion del proyecto y de home controller

---
+++ src/test/java/AlquilerdeAutos/AlquilerdeAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package AlquilerdeAutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerdeAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
