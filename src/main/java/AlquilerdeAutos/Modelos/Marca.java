b55684fca3e866b155259f8070719ca89ca007d5
Alexandra <ha25003@esfe.agape.edu.sv>
2026-08-15 22:15:33 -0600

    Agrege las entiades de Marca y Categoria.

--- src/main/java/AlquilerdeAutos/Modelos/Marca.java
+++ src/main/java/AlquilerdeAutos/Modelos/Marca.java
@@ -1,6 +1,11 @@
 package AlquilerdeAutos.Modelos;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -13,7 +18,7 @@ public class Marca {
     private Integer idMarca;
 
     @NotBlank(message = "El Nombre es Requerido")
-    @Column(name = "Nombre", length = 50, nullable = false)
+    @Column(name = "Nombre")
     private String nombre;
 
     public Marca() {
